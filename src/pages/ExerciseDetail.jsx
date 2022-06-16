849e4a725d22672c318283f2c37e8452a26b2ca6
kotschroedingera <79150323671@ya.ru>
2022-06-16 17:40:31 +0300

    minor changes

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Detail from '../components/Detail';
+import ExerciseVideo from '../components/ExerciseVideo';
+import SimilarExercises from '../components/SimilarExercises';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const youtubeSearchUrl = '';
+      const exerciseDetailData = await fetchData(
+        `https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExerciseData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideo />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
