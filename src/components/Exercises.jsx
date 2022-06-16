0183b68d30fdb67bbf58541fb226a30e2094bb92
kotschroedingera <79150323671@ya.ru>
2022-06-16 17:22:24 +0300

    Update Exercises.jsx

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,5 +1,6 @@
 import { Box, Pagination, Stack, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
@@ -7,9 +8,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const exercisesPerPage = 9;
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
-  const inddexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
   const currentExercises = exercises.slice(
-    inddexOfFirstExercise,
+    indexOfFirstExercise,
     indexOfLastExercise
   );
 
@@ -17,6 +18,25 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     setCurrentPage(value);
     window.scrollTo({ top: 1800, behavior: 'smooth' });
   };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+      if (bodyPart === 'all') {
+        exerciseData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        );
+      } else {
+        exerciseData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+      setExercises(exerciseData);
+    };
+    fetchExercisesData();
+  }, [bodyPart]);
 
   return (
     <Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
