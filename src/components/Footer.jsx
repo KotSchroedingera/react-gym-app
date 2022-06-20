a4216fa3e5092829b3161a7fb20c381db50d51fc
kotschroedingera <79150323671@ya.ru>
2022-06-20 11:18:42 +0300

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
+import LogoImage from '../assets/images/Logo-1.png';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={LogoImage} alt='logo' width='200px' height='40px' />
+        <Typography>Made by AWSCH with the help of Internet.</Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
